9603e0b6590c28c91ea3c11961938a0b0ba41567
trujilml <mickeytrj@gmail.com>
2021-12-14 20:59:54 -0600

    created and changed routes for category model - tested successfully in Insomnia

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,37 +7,61 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-
+    include: [
+      {
+      model: Product
+      } 
+    ]
   })
+  .then(data => {
+    console.log('route to find all categories', data);
+    res.json(data)});
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({
-
+      where: {
+        id: req.params.id
+      },
+      include: [
+      {
+        model: Product
+      }
+    ]
   })
+  .then(data => {
+    console.log('route to find category by id', data);
+    res.json(data)});
 });
 
 router.post('/', (req, res) => {
   // create a new category
-  Category.create({
-
-  })
+  Category.create(req.body)
+  .then(data => {
+    console.log('route to create a category', data);
+    res.json(data)});
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  Category.update({
-
+  Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
   })
+  .then(data => {res.json(data)});
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
   Category.destroy({
-    
+    where: {
+      id: req.params.id
+    }
   })
+  .then(data => {res.json(data)});
 });
 
 module.exports = router;
