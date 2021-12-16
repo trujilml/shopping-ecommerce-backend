858566d0acd3dce271bf2b5b7ecb3f4915676bbf
trujilml <mickeytrj@gmail.com>
2021-12-16 00:14:00 -0600

    added and completed the tag routes - look to requirements on what to do next

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,62 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(data => {
+    console.log('route to find all tags', data);
+    res.json(data)});
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(data => {
+    console.log('route to find the id of a single tag', data);
+    res.json(data)});
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then(data => {
+    console.log('route to create a new tag', data);
+    res.json(data)});
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {res.json(data)});
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {res.json(data)});
 });
 
 module.exports = router;
